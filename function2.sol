1a2141b250b6280be8e7a7648ffb81b35981ed20
Aditya <144545294+Aditya-0510@users.noreply.github.com>
2024-10-06 22:57:45 +0530

    added function2

---
+++ function2.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+// Code copied from optimism
+// https://github.com/ethereum-optimism/optimism/blob/develop/packages/contracts-bedrock/src/dispute/lib/LibUDT.sol
+
+type Duration is uint64;
+
+type Timestamp is uint64;
+
+type Clock is uint128;
+
+library LibClock {
+    function wrap(Duration _duration, Timestamp _timestamp)
+        internal
+        pure
+        returns (Clock clock_)
+    {
+        assembly {
+            // data | Duration | Timestamp
+            // bit  | 0 ... 63 | 64 ... 127
+            clock_ := or(shl(0x40, _duration), _timestamp)
+        }
+    }
+
+    function duration(Clock _clock)
+        internal
+        pure
+        returns (Duration duration_)
+    {
+        assembly {
+            duration_ := shr(0x40, _clock)
+        }
+    }
+
+    function timestamp(Clock _clock)
+        internal
+        pure
+        returns (Timestamp timestamp_)
+    {
+        assembly {
+            timestamp_ := shr(0xC0, shl(0xC0, _clock))
+        }
+    }
+}
+
+
