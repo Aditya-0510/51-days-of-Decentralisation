362aa4152b80a5ceaf527b68217bddaa73283a61
Aditya <144545294+Aditya-0510@users.noreply.github.com>
2024-10-14 22:58:03 +0530

    learned about structs

--- structs.sol
+++ structs.sol
@@ -6,3 +6,21 @@ struct Todo {
     string text;
     bool completed;
 }
+
+Todo[] public todos;
+
+    function create(string calldata _text) public {
+        // 3 ways to initialize a struct
+        // - calling it like a function
+        todos.push(Todo(_text, false));
+
+        // key value mapping
+        todos.push(Todo({text: _text, completed: false}));
+
+        // initialize an empty struct and then update it
+        Todo memory todo;
+        todo.text = _text;
+        // todo.completed initialized to false
+
+        todos.push(todo);
+    }
