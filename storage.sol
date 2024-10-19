1eec17307af7cc2ff77bf911b608c7d5b4f8a67f
Aditya <144545294+Aditya-0510@users.noreply.github.com>
2024-10-19 22:30:50 +0530

    learned storage

---
+++ storage.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.8.26;
+
+contract DataLocations {
+    uint256[] public arr;
+    mapping(uint256 => address) map;
+
+    struct MyStruct {
+        uint256 foo;
+    }
+
+    mapping(uint256 => MyStruct) myStructs;
+
+    function f() public {
+        // call _f with state variables
+        _f(arr, map, myStructs[1]);
+
+        // get a struct from a mapping
+        MyStruct storage myStruct = myStructs[1];
+        // create a struct in memory
+        MyStruct memory myMemStruct = MyStruct(0);
+    }
