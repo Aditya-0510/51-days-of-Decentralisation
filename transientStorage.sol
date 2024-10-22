53ec110015e50205650393e5b3a798bc4d53b8fa
Aditya <144545294+Aditya-0510@users.noreply.github.com>
2024-10-22 22:53:24 +0530

    learnt new things

---
+++ transientStorage.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.8.26;
+
+
+// Storage - data is stored on the blockchain
+// Memory - data is cleared out after a function call
+// Transient storage - data is cleared out after a transaction
+
+interface ITest {
+    function val() external view returns (uint256);
+    function test() external;
+}
+
+contract Callback {
+    uint256 public val;
+
+    fallback() external {
+        val = ITest(msg.sender).val();
+    }
+
+    function test(address target) external {
+        ITest(target).test();
+    }
+}
