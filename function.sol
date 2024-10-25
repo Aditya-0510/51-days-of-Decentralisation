d64e0e0dcce1765309a6e3f1b195320633085155
Aditya <144545294+Aditya-0510@users.noreply.github.com>
2024-10-25 12:00:00 +0530

    Learnt new things

---
+++ function.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.8.26;
+
+contract Function {
+    // Functions can return multiple values.
+    function returnMany() public pure returns (uint256, bool, uint256) {
+        return (1, true, 2);
+    }
+
+    // Return values can be named.
+    function named() public pure returns (uint256 x, bool b, uint256 y) {
+        return (1, true, 2);
+    }
+
+    // Return values can be assigned to their name.
+    // In this case the return statement can be omitted.
+    function assigned() public pure returns (uint256 x, bool b, uint256 y) {
+        x = 1;
+        b = true;
+        y = 2;
+    }
+}
