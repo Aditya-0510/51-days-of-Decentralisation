87dceb0d73494e1ba34ddcdf3f6ece75b15e896d
Aditya <144545294+Aditya-0510@users.noreply.github.com>
2024-10-28 22:23:47 +0530

    created contract

--- function.sol
+++ function.sol
@@ -42,3 +42,28 @@ contract Function {
         return arr;
     }
 }
+contract XYZ {
+    function someFuncWithManyInputs(
+        uint256 x,
+        uint256 y,
+        uint256 z,
+        address a,
+        bool b,
+        string memory c
+    ) public pure returns (uint256) {}
+
+    function callFunc() external pure returns (uint256) {
+        return someFuncWithManyInputs(1, 2, 3, address(0), true, "c");
+    }
+
+    function callFuncWithKeyValue() external pure returns (uint256) {
+        return someFuncWithManyInputs({
+            a: address(0),
+            b: true,
+            c: "c",
+            x: 1,
+            y: 2,
+            z: 3
+        });
+    }
+}
