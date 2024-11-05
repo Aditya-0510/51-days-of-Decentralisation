e9f720d95d8caf2fbb49e0181f171aaaafe82bf5
Aditya <144545294+Aditya-0510@users.noreply.github.com>
2024-11-05 21:50:25 +0530

    learnt new things

--- events.sol
+++ events.sol
@@ -14,3 +14,25 @@ contract Event {
         emit AnotherLog();
     }
 }
+contract EventDrivenArchitecture {
+    event TransferInitiated(
+        address indexed from, address indexed to, uint256 value
+    );
+    event TransferConfirmed(
+        address indexed from, address indexed to, uint256 value
+    );
+
+    mapping(bytes32 => bool) public transferConfirmations;
+
+    function initiateTransfer(address to, uint256 value) public {
+        emit TransferInitiated(msg.sender, to, value);
+    }
+
+    function confirmTransfer(bytes32 transferId) public {
+        require(
+            !transferConfirmations[transferId], "Transfer already confirmed"
+        );
+        transferConfirmations[transferId] = true;
+        emit TransferConfirmed(msg.sender, address(this), 0);
+    }
+}
